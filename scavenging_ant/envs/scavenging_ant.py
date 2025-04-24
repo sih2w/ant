58415cd040816ba77648a46532df4ea121973467
sih2w <sih2w@mtmail.mtsu.edu>
2025-04-24 17:41:41 -0500

    Fixed seed implementation and added Q-learning

--- scavenging_ant/envs/scavenging_ant.py
+++ scavenging_ant/envs/scavenging_ant.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 import math
 import numpy as np
 import pygame
-from gymnasium.spaces import Discrete, Box
+from gymnasium.spaces import Discrete, Box, Dict
 from gymnasium import Env
 
 AGENT_ACTIONS = [
@@ -24,7 +24,10 @@ class Positionable:
         return positions
 
     def set_position(self, position: [int, int]):
-        self.__position = position
+        if isinstance(position, np.ndarray):
+            self.__position = position.tolist()
+        else:
+            self.__position = position
 
     def get_position(self):
         return self.__position
@@ -34,11 +37,9 @@ class Food(Positionable):
             self,
             weight: float = 0.10,
             position: [int, int] = None,
-            aroma_radius: float = 1,
             square_pixel_width: float = 0
     ):
         super().__init__(position)
-        self.__aroma_radius = aroma_radius
         self.__hidden = False
         self.__carried = False
         self.__weight = weight
@@ -67,9 +68,6 @@ class Food(Positionable):
     def set_hidden(self, hidden: bool):
         self.__hidden = hidden
 
-    def get_aroma_radius(self):
-        return self.__aroma_radius
-
 class Nest(Positionable):
     def __init__(
             self,
@@ -78,40 +76,20 @@ class Nest(Positionable):
     ):
         super().__init__(position)
         self.__capacity = capacity
-        self.__agents_occupied = []
         self.__position = position
-
-    def add_agent(self, agent: Agent) -> bool:
-        if len(self.__agents_occupied) < self.__capacity:
-            self.__agents_occupied.append(agent)
-            return True
-        return False
-
-    def remove_agent(self, agent: Agent):
-        self.__agents_occupied.remove(agent)
 
 class Agent(Positionable):
     def __init__(
             self,
             carry_capacity: float = 0.10,
             position: [int, int] = None,
-            scent_radius: float = 1,
-            vision_radius: float = 1
     ):
         super().__init__(position)
         self.__carry_capacity = carry_capacity
-        self.__scent_radius = scent_radius
-        self.__vision_radius = vision_radius
         self.__carried_food = None
 
     def get_carry_capacity(self):
         return self.__carry_capacity
-
-    def get_scent_radius(self):
-        return self.__scent_radius
-
-    def get_vision_radius(self):
-        return self.__vision_radius
 
     def set_carried_food(self, food: Food or None):
         self.__carried_food = food
@@ -147,9 +125,6 @@ class ScavengingAntEnv(Env):
             nest_count: int = 1,
             food_count: int = 1,
             seed: int = np.random.randint(1, 1000),
-            persistent_obstacles: bool = False,
-            persistent_nests: bool = False,
-            persistent_food: bool = False,
     ):
         self.__grid_width = grid_width
         self.__grid_height = grid_height
@@ -163,33 +138,25 @@ class ScavengingAntEnv(Env):
         self.__window = None
         self.__clock = None
         self.__random = np.random.default_rng(seed)
-        self.__persistent_obstacles = persistent_obstacles
-        self.__persistent_nests = persistent_nests
-        self.__persistent_food = persistent_food
 
         self.render_mode = render_mode
         self.render_fps = render_fps
         self.action_space = Discrete(len(AGENT_ACTIONS))
+        self.observation_space = Dict({
+            "position": Box(
+                low=np.array([-1, -1]),
+                high=np.array([self.__grid_width, self.__grid_height]),
+                shape=(2, ),
+                dtype=np.int16
+            ),
+            "at_obstacle": Discrete(2),
+            "at_food": Discrete(2),
+            "at_nest": Discrete(2),
+            "carrying_food": Discrete(2),
+        })
 
-        """
-        1. Agent position plane
-        2. Obstacle position plane
-        3. Nest position plane
-        4. Food position plane
-        5. Is carrying food plane
-        """
-        self.observation_space = Box(
-            low=0,
-            high=np.iinfo(np.int16).max,
-            shape=(5, grid_height, grid_width),
-            dtype=np.int16
-        )
-
-        max_agent_radius = max(grid_width, grid_height)
         self.__agent = Agent(
-            carry_capacity=np.random.uniform(low=0, high=0.50) * max_agent_radius,
-            scent_radius=np.random.uniform(low=0, high=0.50) * max_agent_radius,
-            vision_radius=np.random.uniform(low=0, high=0.50) * max_agent_radius,
+            carry_capacity=np.random.uniform(low=0, high=0.50),
             position=[-1, -1]
         )
 
@@ -201,13 +168,11 @@ class ScavengingAntEnv(Env):
                 )
             )
 
-        max_aroma_radius = max(self.__grid_width, self.__grid_height)
         for _ in range(self.__food_count):
             self.__food.append(
                 Food(
                     weight=np.random.randint(low=1, high=10),
                     position=[-1, -1],
-                    aroma_radius=np.random.uniform(low=0, high=0.50) * max_aroma_radius,
                     square_pixel_width=self.__square_pixel_width
                 )
             )
@@ -243,35 +208,16 @@ class ScavengingAntEnv(Env):
         cell_count = self.__grid_width * self.__grid_height
         return math.floor(cell_count * percent)
 
-    def __coordinates_to_plane(self, coordinates: [[int, int]]):
-        plane = np.zeros(shape=(self.__grid_height, self.__grid_width), dtype=np.int16)
-        for index, coordinate in enumerate(coordinates):
-            plane[coordinate[1], coordinate[0]] += 1
-        return plane
-
-    @staticmethod
-    def __get_in_vision(agent: Agent, items: [Positionable], persistent: bool):
-        coordinates = []
-        agent_position = np.array(agent.get_position())
-        agent_vision_radius = agent.get_vision_radius()
-
-        for index, item in enumerate(items):
-            item_position = np.array(item.get_position())
-            direction = item_position - agent_position
-
-            if persistent or np.linalg.norm(direction) <= agent_vision_radius:
-                coordinates.append(item_position)
-
-        return coordinates
-
     def __get_observation(self):
-        return np.stack((
-            self.__coordinates_to_plane([self.__agent.get_position()]),
-            self.__coordinates_to_plane(self.__get_in_vision(self.__agent, self.__nests, self.__persistent_nests)),
-            self.__coordinates_to_plane(self.__get_in_vision(self.__agent, self.__food, self.__persistent_food)),
-            self.__coordinates_to_plane(self.__get_in_vision(self.__agent, self.__obstacles, self.__persistent_obstacles)),
-            np.zeros(shape=(self.__grid_height, self.__grid_width)) if self.__agent.get_carried_food() is None else np.ones(shape=(self.__grid_height, self.__grid_width))
-        ))
+        agent_position = self.__agent.get_position()
+
+        return {
+            "position": self.__agent.get_position(),
+            "at_obstacle": agent_position in Positionable.get_positions(self.__obstacles),
+            "at_food": agent_position in Positionable.get_positions(self.__food),
+            "at_nest": agent_position in Positionable.get_positions(self.__nests),
+            "carrying_food": self.__agent.get_carried_food() is not None,
+        }
 
     def reset(self, seed: int = None, options = None):
         super().reset(seed=seed)
@@ -288,7 +234,6 @@ class ScavengingAntEnv(Env):
             food.set_position([-1, -1])
 
         self.__agent.set_position([-1, -1])
-
         self.__random = np.random.default_rng(seed=seed)
 
         for obstacle in self.__obstacles:
@@ -391,14 +336,14 @@ class ScavengingAntEnv(Env):
         pygame.draw.circle(
             surface=canvas,
             color=(229, 69, 23),
-            center=(self.__agent.get_position() + 0.50) * self.__square_pixel_width,
+            center=(np.array(self.__agent.get_position()) + 0.50) * self.__square_pixel_width,
             radius=self.__square_pixel_width / 4,
         )
 
     def __draw_food(self, canvas):
         for food in self.__food:
             if not food.is_hidden():
-                position = np.array((food.get_position() + 0.50) * self.__square_pixel_width)
+                position = (np.array(food.get_position()) + 0.50) * self.__square_pixel_width
                 if not food.is_carried():
                     position += food.get_pixel_offset()
 
--- tests/q_learning.py
+++ tests/q_learning.py
@@ -1,129 +1,83 @@
-from collections import defaultdict
-from gymnasium.wrappers import FlattenObservation, RecordEpisodeStatistics
-from tqdm import tqdm
-import scavenging_ant.envs.scavenging_ant as scavenging_ant
 import numpy as np
-import matplotlib.pyplot as plt
+import scavenging_ant.envs.scavenging_ant as scavenging_ant
+from collections import defaultdict
+from tqdm import tqdm
+from gymnasium.wrappers import FlattenObservation, TimeLimit
 
-class QAgent:
-    def __init__(
-        self,
-        env,
-        learning_rate: float,
-        initial_epsilon: float,
-        epsilon_decay: float,
-        final_epsilon: float,
-        discount_factor: float = 0.95,
-    ):
-        self.q_values = defaultdict(lambda: np.zeros(env.action_space.n))
-        self.learning_rate = learning_rate
-        self.discount_factor = discount_factor
-        self.epsilon = initial_epsilon
-        self.epsilon_decay = epsilon_decay
-        self.final_epsilon = final_epsilon
-        self.training_error = []
-
-    def get_action(self, env, observation) -> int:
-        observation = tuple(observation)
-        if np.random.random() < self.epsilon:
-            return env.action_space.sample()
-        else:
-            return int(np.argmax(self.q_values[observation]))
-
-    def update(
-        self,
-        observation,
-        action: int,
-        reward: float,
-        terminated: bool,
-        next_observation,
-    ):
-        observation = tuple(observation)
-        next_observation = tuple(next_observation)
-
-        future_q_value = (not terminated) * np.max(self.q_values[next_observation])
-        temporal_difference = (
-            reward + self.discount_factor * future_q_value - self.q_values[observation][action]
-        )
-
-        self.q_values[observation][action] = (
-            self.q_values[observation][action] + self.learning_rate * temporal_difference
-        )
-        self.training_error.append(temporal_difference)
-
-    def decay_epsilon(self):
-        self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
+def create_env(render_mode: str = None):
+    return scavenging_ant.ScavengingAntEnv(
+        render_mode=render_mode,
+        render_fps=5,
+        grid_height=10,
+        grid_width=18,
+        food_count=1,
+        nest_count=1,
+        percent_obstacles=0,
+        seed=0
+    )
 
 if __name__ == "__main__":
-    learning_rate = 0.001
-    n_episodes = 50
-    start_epsilon = 1.0
-    epsilon_decay = start_epsilon / (n_episodes / 2)
-    final_epsilon = 0.1
+    episodes = 1000
+    seed = 100
 
-    env = scavenging_ant.ScavengingAntEnv(
-        render_mode=None,
-        render_fps=60,
-        persistent_obstacles=True,
-        persistent_food=True,
-        persistent_nests=True,
-        grid_height=10,
-        grid_width=15,
-        food_count=10,
-        percent_obstacles=0.10
-    )
-    
+    env = create_env()
+    env = TimeLimit(env, max_episode_steps=500)
     env = FlattenObservation(env)
-    env = RecordEpisodeStatistics(env, buffer_length=n_episodes)
-    env.reset()
 
-    agent = QAgent(
-        env=env,
-        learning_rate=learning_rate,
-        initial_epsilon=start_epsilon,
-        epsilon_decay=epsilon_decay,
-        final_epsilon=final_epsilon,
-    )
+    q = defaultdict(lambda: np.zeros(env.action_space.n))
+    rewards_per_episode = np.zeros(episodes)
 
-    for episode in tqdm(range(n_episodes)):
-        observation, info = env.reset(seed=0)
-        done = False
+    learning_rate_alpha = 0.10
+    discount_factor_gamma = 0.90
+    epsilon = 1
+    epsilon_decay_rate = epsilon / (episodes / 2)
+    rng = np.random.default_rng()
 
-        while not done:
-            action = agent.get_action(env, observation)
-            next_observation, reward, terminated, truncated, info = env.step(action)
-            agent.update(observation, action, reward, terminated, next_observation)
+    for episode in tqdm(range(episodes)):
+        state = env.reset(seed=seed)[0]
+        state = state.tolist()
+        state = tuple(state)
 
-            done = terminated or truncated
-            observation = next_observation
+        terminated = False
+        truncated = False
 
-        agent.decay_epsilon()
+        while not terminated and not truncated:
+            if rng.random() < epsilon:
+                action = env.action_space.sample()
+            else:
+                action = np.argmax(q[state])
+
+            new_state, reward, terminated, truncated, _ = env.step(action)
+            new_state = tuple(new_state)
+
+            q[state][action] = q[state][action] + learning_rate_alpha * (
+                    reward + discount_factor_gamma * np.max(q[new_state]) - q[state][action]
+            )
+
+            state = new_state
+
+        epsilon = max(epsilon - epsilon_decay_rate, 0)
+        if epsilon == 0:
+            learning_rate_alpha = 0.0001
 
     env.close()
 
-    rolling_length = 500
-    fig, axs = plt.subplots(ncols=3, figsize=(12, 5))
-    axs[0].set_title("Episode rewards")
-    reward_moving_average = (
-            np.convolve(
-                np.array(env.return_queue).flatten(), np.ones(rolling_length), mode="valid"
-            )
-            / rolling_length
-    )
-    axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
-    axs[1].set_title("Episode lengths")
-    length_moving_average = (
-            np.convolve(
-                np.array(env.length_queue).flatten(), np.ones(rolling_length), mode="same"
-            )
-            / rolling_length
-    )
-    axs[1].plot(range(len(length_moving_average)), length_moving_average)
-    axs[2].set_title("Training Error")
-    training_error_moving_average = (
-            np.convolve(np.array(agent.training_error), np.ones(rolling_length), mode="same")
-            / rolling_length
-    )
-    axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
-    plt.tight_layout()
-    plt.show()
+    env = create_env(render_mode="human")
+    env = TimeLimit(env, max_episode_steps=500)
+    env = FlattenObservation(env)
+
+    for episode in range(episodes):
+        state = env.reset(seed=seed)[0]
+        state = state.tolist()
+        state = tuple(state)
+
+        terminated = False
+        truncated = False
+
+        while not terminated and not truncated:
+            action = np.argmax(q[state])
+            new_state, reward, terminated, truncated, _ = env.step(action)
+            new_state = tuple(new_state)
+            state = new_state
+
+    env.close()
