d24059fbd3436d726aef06e902854fe821959f5d
sih2w <sih2w@mtmail.mtsu.edu>
2025-09-22 21:13:42 -0500

    Broke code down into more modules.

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,8 @@
-from typing import Any
+from typing import Any, Callable
 import dill
 import os
 import matplotlib.pyplot as plt
-from scripts.q_learning import train, visualize
+from scripts.q_learning import train, get_greedy_actions
 from scripts.shared import *
 
 
@@ -77,6 +77,97 @@ def plot_episode_data(episode_data: List[Episode]) -> None:
     plt.show()
 
 
+def visualize(
+        state_actions: StateActions,
+        env: ScavengingAntEnv,
+        action_selector: Callable[[StateActions, Dict[AgentName, Observation]], Dict[AgentName, int]],
+) -> None:
+    pygame.init()
+    pygame.display.set_caption("Q-Learning Ants")
+    pygame.display.set_icon(pygame.image.load("../images/icons8-ant-48.png"))
+
+    window_size = env.get_window_size()
+    window = pygame.display.set_mode(window_size)
+    clock = pygame.time.Clock()
+    run_interval_time = 0
+
+    selected_agent_index = 0
+    switching_agent = False
+    auto_run_enabled = False
+    switching_auto_run = False
+    running = True
+    stepping_enabled = False
+    stepping = False
+
+    while running:
+        observations, _ = env.reset()
+        terminations, truncations = {}, {}
+
+        draw(
+            env,
+            observations,
+            selected_agent_index,
+            window,
+            window_size,
+            state_actions,
+        )
+
+        while running and not has_episode_ended(terminations, truncations):
+            draw_next_step = auto_run_enabled and run_interval_time == 0
+            draw_next_step = draw_next_step or (stepping_enabled and not stepping)
+
+            if draw_next_step:
+                stepping = True
+                selected_actions = action_selector(state_actions, observations)
+                observations, rewards, terminations, truncations, info = env.step(selected_actions)
+
+                draw(
+                    env,
+                    observations,
+                    selected_agent_index,
+                    window,
+                    window_size,
+                    state_actions,
+                )
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    break
+            else:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_a]:
+                    if not switching_auto_run:
+                        switching_auto_run = True
+                        auto_run_enabled = not auto_run_enabled
+                        run_interval_time = 0
+                else:
+                    switching_auto_run = False
+
+                if keys[pygame.K_s]:
+                    if not switching_agent:
+                        switching_agent = True
+                        selected_agent_index += 1
+                        if selected_agent_index >= AGENT_COUNT:
+                            selected_agent_index = 0
+                else:
+                    switching_agent = False
+                    if keys[pygame.K_SPACE]:
+                        stepping_enabled = True
+                    else:
+                        stepping_enabled = False
+                        stepping = False
+
+            delta_time = clock.tick(RENDER_FPS) / 1000
+            if auto_run_enabled:
+                run_interval_time += delta_time
+                if run_interval_time >= SECONDS_BETWEEN_AUTO_STEP:
+                    run_interval_time = 0
+
+    pygame.display.quit()
+    pygame.quit()
+
+
 if __name__ == "__main__":
     os.makedirs(name=SAVE_DIRECTORY, exist_ok=True)
 
@@ -95,10 +186,10 @@ if __name__ == "__main__":
     try:
         state_actions, episode_data = load_data()
     except FileNotFoundError:
-        state_actions, episode_data = train(env)
+        state_actions, episode_data = train(env) # Can swap train function
         if SAVE_AFTER_TRAINING:
             save_data(state_actions, episode_data)
     plot_episode_data(episode_data)
 
     if SHOW_AFTER_TRAINING:
-        visualize(state_actions, env)
+        visualize(state_actions, env, get_greedy_actions) # Can swap action selector
